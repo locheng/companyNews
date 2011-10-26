6b161a99210436af610a52580911b9c9f32a6d19
Sebastian Otaegui <feniix@gmail.com>
2011-10-26 16:49:55 -0500

    moved the prevayler data directory to ${user.home}/companynews/data

--- src/com/example/companyblog/BlogListener.java
+++ src/com/example/companyblog/BlogListener.java
@@ -1,6 +1,11 @@
 package com.example.companyblog;
 
 import java.io.IOException;
+import java.io.File;
+import java.lang.RuntimeException;
+import java.lang.String;
+import java.lang.System;
+import java.lang.reflect.Field;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
@@ -12,27 +17,40 @@ import com.example.companyblog.model.Blog;
 
 import static com.example.companyblog.AppConstants.*;
 
+
 public class BlogListener implements ServletContextListener {
-    
+
     Prevayler prevayler;
-    
+
+    public String setupPrevayleyDirectory() {
+        String USER_HOME = System.getProperty("user.home");
+        String DBPATH = USER_HOME + File.separatorChar + "companynews/data";
+        System.out.println("Setting up the path to " + DBPATH);
+        File DBFILE = new File(DBPATH);
+        if (!DBFILE.exists()) {
+            DBFILE.mkdir();
+        }
+        return DBFILE.getAbsolutePath();
+    }
+
     public void contextInitialized(ServletContextEvent event) {
         System.out.println("Setting up Prevayler.");
         try {
-            prevayler = PrevaylerFactory.createPrevayler(new Blog(), "/home/russell/persistence/files*");   
+            String PREVAYLENCE_HOME = setupPrevayleyDirectory();
+            prevayler = PrevaylerFactory.createPrevayler(new Blog(), PREVAYLENCE_HOME);
             event.getServletContext().setAttribute(PREVAYLER_KEY, prevayler);
         } catch (Exception e) {
             e.printStackTrace();
             throw new RuntimeException(e);
         }
     }
-    
+
     public void contextDestroyed(ServletContextEvent event) {
         System.out.println("Tearing down Prevayler.");
         try {
             prevayler.takeSnapshot();
         } catch (IOException e) {
-            e.printStackTrace();            
+            e.printStackTrace();
         }
     }
 }
