2f94a1cc369af1448003d8df9c7454d0e80b92fd
Sebastian Otaegui <feniix@gmail.com>
2011-11-01 10:58:29 -0500

    add support to set and environment variable to set the data directory for prevayler

--- src/main/java/com/example/companyblog/BlogListener.java
+++ src/main/java/com/example/companyblog/BlogListener.java
@@ -19,22 +19,31 @@ import static com.example.companyblog.AppConstants.*;
 public class BlogListener implements ServletContextListener {
 
     Prevayler prevayler;
+    private String dataDir;
 
-    public String setupPrevayleyDirectory() {
-        String USER_HOME = System.getProperty("user.home");
-        String DBPATH = USER_HOME + File.separatorChar + "companynews/data";
+    public void setupPrevayleyDirectory() throws IOException {
+        final String DBDATADIR = System.getenv("DBDATADIR");
+        final String USER_HOME = System.getProperty("user.home");
+        String DBPATH = null;
+
+        if (DBDATADIR.isEmpty()) {
+            DBPATH = new String(USER_HOME + File.separatorChar + "companynews/data");
+        } else {
+            DBPATH = new String(DBDATADIR + File.separatorChar + "companynews/data");
+        }
         System.out.println("Setting up the path to " + DBPATH);
         File DBFILE = new File(DBPATH);
         if (!DBFILE.exists()) {
             DBFILE.mkdir();
         }
-        return DBFILE.getAbsolutePath();
+        this.dataDir = new String(DBFILE.getCanonicalPath());
     }
 
     public void contextInitialized(ServletContextEvent event) {
         System.out.println("Setting up Prevayler.");
         try {
-            String PREVAYLENCE_HOME = setupPrevayleyDirectory();
+            setupPrevayleyDirectory();
+            final String PREVAYLENCE_HOME = new String(this.dataDir);
             prevayler = PrevaylerFactory.createPrevayler(new Blog(), PREVAYLENCE_HOME);
             event.getServletContext().setAttribute(PREVAYLER_KEY, prevayler);
         } catch (Exception e) {
